2b46bfbbfb9daea1105faac58b8949519f14dfea
Ruslan <gimazevs@mail.ru>
2022-10-23 16:28:44 +0300

    третий коммит

--- src/Main.java
+++ src/Main.java
@@ -11,30 +11,58 @@ public class Main {
         return checkTheYear;
     }
 
-    public static int choosingSmartphoneSystem(int a, int b) {
+    public static int choosingSmartphoneYear(int a) {
+        int choosingSmartphoneYear = a;
         int currentYear = LocalDate.now().getYear();
         if (a >= 2015 && a <= currentYear) {
-            if (b == 1) {
-                System.out.println("Установите версию приложения для Android по ссылке");
-            } else if (b == 0) {
-                System.out.println("Установите версию приложения для iOS по ссылке");
-            }
-        } else if (b == 1) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (b == 0) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                System.out.print("Установите версию приложения по ссылке");
+        } else {
+            System.out.print("Установите облегченную версию приложения по ссылке");
         }
-        return choosingSmartphoneSystem(a,b);
+        return choosingSmartphoneYear;
     }
+    public static int choosingSmartphoneSystem(int a) {
+        int choosingSmartphoneSystem = a;
+        if (a == 1) {
+            System.out.print(" для Андроид ");
+        } else if (a == 0) {
+            System.out.print(" для IOS ");
+        }
+        return choosingSmartphoneSystem;
+    }
+    public static int numberOfDeliveryDays (int deliveryDistance) {
+        int numberOfDeliveryDays = 0;
+        if (deliveryDistance <= 20) {
+            numberOfDeliveryDays = numberOfDeliveryDays + 1;
+            System.out.print("Потребуется для доставки дня - ");
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            numberOfDeliveryDays = numberOfDeliveryDays + 2;
+            System.out.print("Потребуется для доставки дня - ");
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            numberOfDeliveryDays = numberOfDeliveryDays + 3;
+            System.out.print("Потребуется для доставки дня - ");
+        } else if (deliveryDistance > 100 && deliveryDistance <= 140) {
+            numberOfDeliveryDays = numberOfDeliveryDays + 4;
+            System.out.print("Потребуется для доставки дня - ");
+        }
+        return numberOfDeliveryDays;
+    }
+
     public static void main(String[] args) {
         //задание 1
         int year = 2000;
         int conclusion = checkTheYear(year);
         System.out.println(conclusion);
         //задание 2
-        int currYear = 2010;
-        int clientOS = 1;
-        int systemOS = choosingSmartphoneSystem(currYear, clientOS);
-        System.out.println(systemOS);
+        int currYear = 2017;
+        int clientOS = 0;
+        int systemYear = choosingSmartphoneYear(currYear);
+        int systemOS = choosingSmartphoneSystem(clientOS);
+        System.out.println(systemYear + systemOS);
+        //задание 3
+        int deliveryDistance = 100;
+        int numberOfDeliveryDays = numberOfDeliveryDays(deliveryDistance);
+        System.out.println(numberOfDeliveryDays);
+
     }
 }
