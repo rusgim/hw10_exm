ed76fd96ae94593474c379260daf09b87a0616f4
Ruslan <gimazevs@mail.ru>
2022-10-23 15:38:43 +0300

    Второй коммит, с ошибкой. Подскажите пожалуйста как решить эту задачу

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,40 @@
+import java.time.LocalDate;
+
 public class Main {
-    public static int sum(int numbers) {
-        int sum = numbers;
+    public static int checkTheYear(int numbers) {
+        int checkTheYear = numbers;
         if (numbers % 4 == 0 && numbers % 100 != 0 || numbers % 400 == 0) {
-            System.out.println("Високосный год");
+            System.out.print("Високосный год ");
         } else {
-            System.out.println("Не високосный год");
+            System.out.print("Не високосный год ");
         }
-        return sum;
+        return checkTheYear;
+    }
+
+    public static int choosingSmartphoneSystem(int a, int b) {
+        int currentYear = LocalDate.now().getYear();
+        if (a >= 2015 && a <= currentYear) {
+            if (b == 1) {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            } else if (b == 0) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        } else if (b == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (b == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        return choosingSmartphoneSystem(a,b);
     }
     public static void main(String[] args) {
+        //задание 1
         int year = 2000;
-        int total = sum(year);
-        System.out.println(total);
+        int conclusion = checkTheYear(year);
+        System.out.println(conclusion);
+        //задание 2
+        int currYear = 2010;
+        int clientOS = 1;
+        int systemOS = choosingSmartphoneSystem(currYear, clientOS);
+        System.out.println(systemOS);
     }
 }
